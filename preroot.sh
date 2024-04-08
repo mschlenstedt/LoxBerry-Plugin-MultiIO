3c5e6390f05d8b32977ab9b988801763be0feb49
Michael Schlenstedt <Michael@schlenn.net>
2024-04-08 07:28:08 +0200

    Bugfix installing Python adxl345 module

--- preroot.sh
+++ preroot.sh
@@ -67,7 +67,12 @@ else
 fi 
 
 echo "<INFO> Start installing Python Modules..."
-yes | python3 -m pip install --upgrade RPi.GPIO pcf8575 pcf8574 adafruit_circuitpython_mcp230xx adafruit-circuitpython-ads1x15 adafruit-circuitpython-ahtx0 smbus2 RPi.bme280 bme680 w1thermsensor pi-ina219 adafruit-mcp3008 ads1x15
+yes | python3 -m pip install --upgrade RPi.GPIO pcf8575 pcf8574 adafruit_circuitpython_mcp230xx adafruit-circuitpython-ads1x15 adafruit-circuitpython-ahtx0 smbus2 RPi.bme280 bme680 w1thermsensor pi-ina219 adafruit-mcp3008
+
+# SPecial handling because Module is too old...
+yes | python3 -m pip install "setuptools<58.0.0" wheel
+yes | python3 -m pip install adxl345
+yes | python3 -m pip install setuptools
 
 # Following packages are for some special boards only
 if [ -e /boot/dietpi/.hw_model ]; then
